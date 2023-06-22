631a2bebdcb8f252b55b2419999b7ba4f4ee391a
fazlikziyaev <fazliddin2002.15@gmail.com>
2023-06-23 02:00:25 +0500

    Initial Commit

---
+++ FreeExchangeRates/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreeExchangeRates
+//
+//  Created by Fazlik Ziyaev on 23/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
